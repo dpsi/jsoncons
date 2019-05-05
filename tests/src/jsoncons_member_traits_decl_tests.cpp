eb292d333efa79d6ed9bb456864d965569a91321
danielaparker <danielaparker@yahoo.com>
2019-05-05 13:55:19 -0400

    Add Valgrind memory support

--- tests/src/jsoncons_member_traits_decl_tests.cpp
+++ tests/src/jsoncons_member_traits_decl_tests.cpp
@@ -35,8 +35,8 @@ namespace jsoncons_member_traits_decl_tests {
  
 namespace ns = jsoncons_member_traits_decl_tests;
 
-JSONCONS_MEMBER_TRAITS_DECL(ns::book,author,title,price);
-JSONCONS_MEMBER_TRAITS_DECL(ns::book2,author,title,price,isbn);
+JSONCONS_MEMBER_TRAITS_DECL(ns::book,author,title,price)
+JSONCONS_MEMBER_TRAITS_DECL(ns::book2,author,title,price,isbn)
 
 TEST_CASE("JSONCONS_MEMBER_TRAITS_DECL tests")
 {
