daa138c4d3032912636548f55dd3ee2ee9bb0680
danielaparker <danielaparker@yahoo.com>
2019-06-20 12:52:04 -0400

    buffered_staj_reader

--- include/jsoncons/buffered_staj_reader.hpp
+++ include/jsoncons/buffered_staj_reader.hpp
@@ -20,50 +20,53 @@
 #include <jsoncons/parse_error_handler.hpp>
 #include <jsoncons/ser_context.hpp>
 #include <jsoncons/result.hpp>
+#include <jsoncons/json.hpp> // basic_json
 #include <jsoncons/staj_reader.hpp>
 #include <jsoncons/detail/print_number.hpp>
 
 namespace jsoncons {
 
 template<class CharT>
-class basic_staj_event_holder
-{
-    std::vector<unsigned char> memory_;
-    basic_staj_event<CharT> event_;
-
-public: 
-    basic_staj_event_holder(const basic_staj_event<CharT>& event)
-    {
-        switch (event.event_type())
-        {
-            case staj_event_type::string_value:
-            {
-                auto v = event.as<basic_string_view<CharT>();
-                memory_.resize(v.length()*sizeof(CharT)+sizeof(CharT));
-                event_ = basic_staj_event<CharT>(new (&memory_[0])CharT[v.size()],
-                                                 v.size(),
-                                                 event.event_type(),
-                                                 event.get_semantic_tag());
-                break;
-            }
-            case staj_event_type::byte_string_value:
-                break;
-            default:
-                event_ = event;
-                break;
-        }
-    }
-
-};
-
-template<class CharT>
 class basic_buffered_staj_reader : public basic_staj_reader<CharT>
 {
+    typedef basic_json<CharT> json_type;
+    typedef std::basic_string<CharT> string_type;
+
+    struct stack_item
+    {
+        template <class... Args>
+        stack_item(std::true_type, Args&& ... args)
+            : name_(std::forward<Args>(args)...)
+        {
+        }
+        template <class... Args>
+        stack_item(std::false_type, Args&& ... args)
+            : value_(std::forward<Args>(args)...)
+        {
+        }
+
+        stack_item() = default;
+        stack_item(const stack_item&) = default;
+        stack_item(stack_item&&) = default;
+        stack_item& operator=(const stack_item&) = default;
+        stack_item& operator=(stack_item&&) = default;
+
+        string_type name_;
+        json_type value_;
+    };
+
+    enum class structure_type {root_t, array_t, object_t};
+
     basic_staj_reader<CharT>* reader_;
+    std::vector<stack_item> item_stack_;
+    std::vector<structure_type> structure_stack_;
 public:
     basic_buffered_staj_reader(basic_staj_reader<CharT>& reader)
         : reader_(std::addressof(reader))
     {
+        item_stack_.reserve(1000);
+        structure_stack_.reserve(100);
+        structure_stack_.push_back(structure_type::root_t);
     }
 
     bool done() const override
